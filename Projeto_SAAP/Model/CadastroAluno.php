e06cbf528be74ddc9a8897d9a8e895e3cf9906a0
Marcos-Cassiano <mcsbrigida@hotmail.com>
2017-11-22 13:19:01 -0200

    Estrutura Inicial do Projeto

---
+++ Projeto_SAAP/Model/CadastroAluno.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @projeto Projeto_SAAP
+ * @Nome CadastroAluno
+ * @Descrição
+ * @copyright (c) 21/11/2017, Marcos Cassiano
+ * @author Marcos Cassiano Santa Brigida
+ * @email mcsbrigida@hotmail.com
+ */
+class CadastroAluno {
+    //put your code here
+}
---
+++ Projeto_SAAP/Model/CadastroProfessor.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @projeto Projeto_SAAP
+ * @Nome CadastroProfessor
+ * @Descrição
+ * @copyright (c) 21/11/2017, Marcos Cassiano
+ * @author Marcos Cassiano Santa Brigida
+ * @email mcsbrigida@hotmail.com
+ */
+class CadastroProfessor {
+    //put your code here
+}
